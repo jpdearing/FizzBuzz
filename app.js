92a92ae10f48303ea0bde6759acd8fd30e9399c8
JPDearing <justin.p.dearing@gmail.com>
2018-07-20 20:50:29 -0500

    Fixed #2 #3
    Solution for the overall FizzBuzz challenge

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 var num = process.argv[2];
 
-if (num % 3 === 0) {
+if (num % 15 === 0) {
+    console.log("fizzbuzz")
+} else if (num % 5 === 0) {
+    console.log("buzz")
+} else if (num % 3 === 0) {
     console.log("fizz")
 } else {
-    console.log(num)
-
+    console.log(num);
 };
